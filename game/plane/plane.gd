e37ef6bb3c4086569c302903035b85d6e7e0d734
knyaz <saba2006.diasamidze@gmail.com>
2025-02-28 11:55:40 +0400

    added the plane fall mode

--- game/plane/plane.gd
+++ game/plane/plane.gd
@@ -1,14 +1,24 @@
 extends RigidBody2D
 
+var mode
 
 func _ready() -> void:
 	gravity_scale = 0
+	mode = "fly"
 
 func _process(delta: float) -> void:
-	fly()
+	match mode:
+		"fly":
+			fly()
+		"fall":
+			fall()
 
 func fly():
 	linear_velocity.x = 50
 	
 func fall():
-	gravity_scale = 1 
+	linear_velocity.y = 40
+
+#check for collisions with explosion instance.
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	mode = "fall"
