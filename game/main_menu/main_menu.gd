f15886ee74369760f178f2680842521293b923ce
knyaz <saba2006.diasamidze@gmail.com>
2025-03-11 13:10:58 +0400

    added the settings menu (unfinished)

--- game/main_menu/main_menu.gd
+++ game/main_menu/main_menu.gd
@@ -22,8 +22,7 @@ func _on_texture_button_pressed() -> void:
 
 
 func _on_texture_button_2_pressed() -> void:
-	pass #this line swiches to options menu. 
-
+	get_tree().change_scene_to_file("res://game/settings/settings.tscn")
 
 func _on_texture_button_3_pressed() -> void:
 	get_tree().quit()
